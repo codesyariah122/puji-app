cfc7c87e991270ddbdad45b4ae6fac1224b14fc7
codesyariah122 <pujiermanto@gmail.com>
2021-03-10 13:13:03 +0700

    update artwork

--- views/src/components/Hero.jsx
+++ views/src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = props => {
 		<h4 className="hero-heading">
 			{props.label}
 		</h4>
-		<a className="button button-primary" href={props.buttonUrl}>{props.buttonLabel}
+		<a className="button button-secondary" href={props.buttonUrl}>{props.buttonLabel}
 		</a>
 	</Fragment>
 	)
