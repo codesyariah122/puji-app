a56ec46576f386c7266829268b99fbca115e04dc
pujiermanto <pujiermanto@gmail.com>
2021-05-30 06:38:29 +0700

    Update index.js

--- Server/App/Routers/web/index.js
+++ Server/App/Routers/web/index.js
@@ -2,7 +2,12 @@ import Router from 'express'
 import { getHome, getContact } from '../../Controllers/index.js'
 
 const WebRouter = Router()
-
+WebRouter.get('*',function(req,res,next){
+  if(req.headers['x-forwarded-proto']!='https')
+    res.redirect('https://puji-app.herokuapp.com'+req.url)
+  else
+    next() /* Continue to other routes if we're not redirecting */
+})
 WebRouter.get('/', getHome)
 WebRouter.get('/contact', getContact)
 
